5bcff819f4886794202e8f47773a4157e9b79fa1
Tony Li Xu <txu@Tonys-MBP.home>
2018-04-30 22:02:24 -0400

    Added default and override attributes

--- roles/web_server.rb
+++ roles/web_server.rb
@@ -1,4 +1,8 @@
 name "nginx_server"
 description "A role to configure nginx proxy servers"
 run_list "recipe[nginx]"
+# If node is part of prod, run "config_prod" recipe
+# Otherwise run config_test
 env_run_lists "production" => ["recipe[nginx::config_prod]"], "testing" => ["recipe[nginx::config_test]"]
+default_attributes "nginx" => { "log_location" => "/var/log/nginx.log" }
+override_attributes "nginx" => { "gzip" => "on" }
