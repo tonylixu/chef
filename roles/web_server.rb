2c8b29b785a5115be6c050a1eb91ad00e72a680c
Tony Li Xu <txu@Tonys-MBP.home>
2018-04-30 22:00:57 -0400

    Added env specific run lists

--- roles/web_server.rb
+++ roles/web_server.rb
@@ -1,3 +1,4 @@
 name "nginx_server"
 description "A role to configure nginx proxy servers"
 run_list "recipe[nginx]"
+env_run_lists "production" => ["recipe[nginx::config_prod]"], "testing" => ["recipe[nginx::config_test]"]
