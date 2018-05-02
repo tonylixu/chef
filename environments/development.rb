3bfbfc586e1a6cfcad45d53b9f8ad072f828dfae
Tony Li Xu <txu@Tonys-MBP.home>
2018-05-01 21:27:25 -0400

    Added env override attributes

--- environments/development.rb
+++ environments/development.rb
@@ -1,5 +1,14 @@
 name "development"
+description "The development env"
 cookbook_versions({
     "nginx" => "<= 1.1.0",
     "apt" => "= 0.0.1"
+})
+override_attributes ({
+    "nginx" => {
+        "listen" => [ "80", "443" ]
+    },
+    "mysql" => {
+        "root_pass" => "root"
+    }
 })
